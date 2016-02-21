b882445001b5eb334bfa479fb953b9f0ad186b19
HARADA REO <rharada@golyat.local>
2016-02-21 21:48:42 +0900

    Initial Commit

---
+++ 天気アプリ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  天気アプリ
+//
+//  Created by HARADA REO on 2016/02/21.
+//  Copyright © 2016年 reo harada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
